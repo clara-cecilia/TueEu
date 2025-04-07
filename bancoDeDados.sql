6bf6544fd556ad59a98541be1013d7dade2d6f74
Bruno Sales Maia <145605639+bruno-s-m@users.noreply.github.com>
2025-04-07 20:47:43 -0300

    Create bancoDeDados.sql
    codigo sql

---
+++ bancoDeDados.sql
@@ -0,0 +1,20 @@
+USE cadastro_tueeu;
+
+CREATE TABLE usuario (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    sexo ENUM('Masculino', 'Feminino') NOT NULL,
+    cpf CHAR(11) NOT NULL UNIQUE,
+    data_nasc DATE NOT NULL,
+    telefone CHAR(11),
+    pais VARCHAR(50) NOT NULL,
+    estado CHAR(2) NOT NULL,
+    cidade VARCHAR(50) NOT NULL,
+    bairro VARCHAR(50) NOT NULL,
+    cep CHAR(8) NOT NULL,
+    endereco VARCHAR(255) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    senha VARCHAR(255) NOT NULL
+);
+
+ALTER TABLE usuario ADD is_admin TINYINT DEFAULT 0;
