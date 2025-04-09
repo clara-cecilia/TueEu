cf4f23fd5f94171b3554d8798c678a0ff103e274
Bruno Sales Maia <145605639+bruno-s-m@users.noreply.github.com>
2025-04-09 17:21:22 -0300

    Update app.py
    criacao hash do email teste

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template, request, redirect, url_for, session, flash
 import mysql.connector
 from bcrypt import hashpw, gensalt , checkpw
+import hashlib
 
 app = Flask(__name__)
 app.secret_key = 'chave_secreta' # Necessária para usar sessões
-
+#
+#conexão com banco de dados 
+#
 try:
     db = mysql.connector.connect(
         host="localhost",
@@ -17,8 +20,9 @@ try:
     print("Conexão realizada com sucesso!")
 except mysql.connector.Error as err:
     print(f"Erro na conexão: {err}")
-
-
+#
+#criação de rotas das paginas 
+#
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -34,10 +38,15 @@ def cadastro():
 @app.route('/esqueceu-senha')
 def esqueceuSenha():
     return render_template('esqueceuSenha.html')
-
+#
+#cadastrar usuario no banco de dados 
+#
 @app.route('/cadastrar', methods=['POST'])
 def cadastrar():
     cursor = db.cursor()
+
+    email = request.form['email']
+    email_hash = hashlib.sha256(email.encode('utf-8')).hexdigest()
 
     dados = (
         request.form['nome'],
@@ -51,7 +60,7 @@ def cadastrar():
         request.form['bairro'],
         request.form['cep'],
         request.form['endereco'],
-        request.form['email'],
+        email_hash,
         request.form['senha']
     )
 
@@ -73,7 +82,7 @@ def cadastrar():
     cursor.close()
     return redirect('/')
 #
-# pagina de login
+# pagina de login, validação do e-mail e senha.
 #
 @app.route('/logar', methods=['POST'])
 def logar():
@@ -192,6 +201,47 @@ def excluir_usuario(id):
     return redirect(url_for('admin'))
 
 #adm editar usuarios
+@app.route('/admin/editar/<int:id>', methods=['GET','POST'])
+def editar_usuario(id):
+    cursor = db.cursor(dictionary=True)
+
+    if request.method == 'POST':
+            nome = request.form['nome']
+            sexo = request.form['sexo']
+            cpf = request.form['cpf']
+            data_nasc = request.form['data_nasc']
+            telefone = request.form['telefone']
+            pais = request.form['pais']
+            estado = request.form['estado']
+            cidade = request.form['cidade']
+            bairro = request.form['bairro']
+            cep = request.form['cep']
+            endereco = request.form['endereco']
+            email = request.form['email']
+            nova_senha = request.form['senha']
+        
+            if nova_senha:
+                senha_criptografada = hashpw(nova_senha.encode('utf-8'), gensalt()).decode('utf-8')
+            else:
+                cursor.execute("SELECT senha FROM usuario WHERE id = %s",(id))
+                senha_criptografada = cursor.fetchone()['senha']
+
+        
+            cursor.execute("""
+                 UPDATE usuario
+                 SET nome=%s, sexo=%s, cpf=%s, data_nasc=%s, telefone=%s, pais=%s, estado=%s, cidade=%s, bairro=%s, cep=%s, endereco=%s, email=%s, senha=%s 
+                WHERE id=%s
+                """,(nome, sexo, cpf, data_nasc, telefone, pais, estado, cidade, bairro, cep, endereco, email, senha_criptografada, id))
+        
+
+            db.commit()
+            flash("Dados do usuario atualizado com sucesso!")
+            return redirect(url_for('admin'))
+    
+    cursor.execute("SELECT * FROM usuario WHERE id = %s",(id,))
+    usuario = cursor.fetchone()
+
+    return render_template('adminEditar.html', usuario=usuario)
 
 
 @app.route('/logout')
